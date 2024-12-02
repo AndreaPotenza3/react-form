7630d787eedb55be05c6ce2e7cba499d5a51c5cf
AndreaPotenza <andreapoten3@gmail.com>
2024-12-02 18:08:45 +0100

    completata aggiunta post

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,40 @@
 import Card from '../post/Card/card'
 import { posts } from '../../posts.jsx'
 import Tags from '../Tags/Tags'
+import { useState } from 'react'
 
 function Main() {
+
+    const [newPosts, setNewPosts] = useState(posts)
+    const [newTitle, setTitle] = useState('Naruto')
+
+    function addPost(e) {
+        e.preventDefault()
+
+        // const newTitle = newTitle.trim()
+
+
+
+        const newPost = {
+            id: Date.now(),
+            title: newTitle,
+            image: undefined /* compila questo campo */,
+            content: '',
+            tags: [],
+            published: true,
+        }
+
+
+
+        if (newTitle === '') return
+
+
+
+        setNewPosts([...newPosts, newPost])
+        setTitle('')
+
+        console.log({ newPost })
+    }
 
     const tags = []
 
@@ -13,19 +45,19 @@ function Main() {
 
         });
     });
-    console.log(tags)
+
     return (
         <>
             <main>
                 <div className='container'>
                     <Tags tags={tags} />
-                    <form className='form' action="submit">
-                        <input className='input_field' type="text" placeholder='Titolo del Post' />
-                        <input className='input_btn' type="submit" name="" id="" />
+                    <form onSubmit={addPost} className='form' action="submit">
+                        <input className='input_field' type="text" onChange={(e) => setTitle(e.target.value)} placeholder='Titolo del Post' value={newTitle} />
+                        <input className='input_btn' type="submit" value='Crea nuovo post' />
                     </form>
                     <div className="row">
 
-                        {posts.map((post) => (
+                        {newPosts.map((post) => (
                             <div key={post.id} className="col-6">
                                 {post.published === true && <Card title={post.title} tags={post.tags} content={post.content} image={post.image} />}
                             </div>
